593e45a7858ff32db1d1ff0bf353034f7d3d712a
AlieZVzz <47358078+AlieZVzz@users.noreply.github.com>
2022-02-17 15:58:56 +0800

    fix most bugs and optimize code

--- Config.py
+++ Config.py
@@ -1,17 +1,16 @@
 class Config(object):
     train_path = 'Dataset/eng_train.txt'
     validation_path = 'Dataset/eng_validation.txt'
-    test_path = 'Dataset/baidu_zh_token.txt'
+    test_path = 'Dataset/eng_validation.txt'
     epoch = 20
 
-    embedding_dim = 300
-    hidden_dim = 100
+    embed_size = 100
+    num_hiddens = 100
     batch_size = 64
     momentum = 0.9
+    num_layers = 2
 
     lr = 1e-3
-
-    layer_size = 2
 
     bidirectional = True
     if bidirectional:
@@ -24,4 +23,3 @@ class Config(object):
     filter_num = 64
     filter_sizes = '3,4,5'
     dropout = 0.2
-
